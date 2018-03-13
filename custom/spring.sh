0ed3d70d037e1c7102de40d0c29fcaaecbc4d8c6
A. Best <adam.best@shopspring.com>
2018-03-13 09:34:33 -0400

    adds ~/code/branded to the GOPATH

--- custom/spring.sh
+++ custom/spring.sh
@@ -1,3 +1,6 @@
 plugins+=( arcanist )
 # Customization of my zshrc 
 export PATH=$PATH:$HOME/tools/arcanist/bin
+export GOPATH=$GOPATH:$HOME/code/branded/go
+export SPRING_IGNORE_GO_ADD_ON_COMMIT=True
+
